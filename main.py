ac9b844577b0fa5a464dc62745d3899cd876746e
paipay <faizahraghnaita@gmail.com>
2026-02-21 07:07:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 print("Project Antrian Tiket Bioskop")
 print("nilai kami bagus")
-<<<<<<< HEAD
 print("kami akan terus belajar untuk meningkatkan nilai kami")
-=======
 print("aku cantik")
->>>>>>> fe763d6205257b78b1a511069ea742efb22f9359
 print("kita pintar")
