c9d43fc2f6f0fe9182d123abe7ae75dd3cca2882
intnshr <intansahara@apps.ipb.ac.id>
2026-02-21 22:06:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ print("aku cantik")
 >>>>>>> fe763d6205257b78b1a511069ea742efb22f9359
 print("kita pintar")
 print("kita pintar")
+#===============================================================
+#Nama Kelompok : ThreeCute
+#Nama dan NIM Anggota : 
+#Kelas : TPL A2
+#===============================================================
+
+print("Terima kasih sudah menggunakan program kami")
